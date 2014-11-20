0ea2b0659ab49a0f167e0e3aab876ea74ecaa23a
Kulanthaivel Palanichamy <kulanthaivel@codeaurora.org>
2014-11-20 18:36:58 -0500

    skia: Add NEON optimization for Skia blit method D32_A8_Black()
    Change-Id: I34058655567dd0409f6e0740ec425e7cacc97ad9
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    
    Conflicts:
    	Android.mk

--- Android.mk
+++ Android.mk
@@ -612,7 +612,8 @@ LOCAL_SRC_FILES_arm += \
 	src/opts/SkBlitRow_opts_arm_neon.cpp \
 	src/opts/SkBlurImage_opts_neon.cpp \
 	src/opts/SkMorphology_opts_neon.cpp \
-	src/opts/SkXfermode_opts_arm_neon.cpp
+	src/opts/SkXfermode_opts_arm_neon.cpp \
+	src/opts/D32_A8_Black_Neon.S
 
 LOCAL_CFLAGS_arm += \
 	-D__ARM_HAVE_NEON
